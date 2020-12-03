4bedf5190a394409c37cd6896b4d945a2076b281
RichoDemus <git@richodemus.com>
2020-12-03 14:22:01 +0100

    Added a bunch of performance optimized solutions for day 3

--- src/day3.rs
+++ src/day3.rs
@@ -67,6 +67,105 @@ pub fn part1_perf(input: &str) -> i32 {
     trees
 }
 
+#[aoc(day3, part1, perf_enumerate)]
+pub fn part1_perf_enumerate(input: &str) -> i32 {
+    let right_step = 3;
+    let mut trees = 0;
+    for (i, line) in input.lines().enumerate() {
+        if line.chars().nth(i * right_step % line.len()).unwrap() == '#' {
+            trees += 1;
+        }
+    }
+    trees
+}
+
+#[aoc(day3, part1, perf_bytes)]
+pub fn part1_perf_bytes(input: &[u8]) -> i32 {
+    let mut desired_column = 0;
+    let mut current_column = 0;
+    let mut width = 0;
+    let mut trees = 0;
+    for byte in input {
+        match byte {
+            35 => {
+                // # is 35
+                if desired_column == current_column {
+                    trees += 1;
+                }
+                current_column += 1;
+            }
+            46 => {
+                // . is 46
+                current_column += 1;
+            }
+            10 => {
+                // 10 is \n
+                if width == 0 {
+                    width = current_column;
+                }
+                current_column = 0;
+                desired_column += 3;
+                if desired_column >= width {
+                    desired_column -= width;
+                }
+            }
+            c => panic!("Unrecognized char: {}", c),
+        };
+    }
+    trees
+}
+
+#[aoc(day3, part1, perf_bytes_split)]
+pub fn part1_perf_bytes_split(input: &[u8]) -> i32 {
+    let right_step = 3;
+
+    let mut column = 0;
+    let mut trees = 0;
+    for line in input.split(|c| c == &10) {
+        if line.is_empty() {
+            continue;
+        }
+        if line.get(column % line.len()).unwrap() == &35 {
+            trees += 1;
+        }
+        column += right_step;
+    }
+    trees
+}
+
+#[aoc(day3, part1, perf_bytes_modulus)]
+pub fn part1_perf_bytes_modulus(input: &[u8]) -> i32 {
+    let mut desired_column = 0;
+    let mut current_column = 0;
+    let mut width = 1;
+    let mut trees = 0;
+    for byte in input {
+        match byte {
+            35 => {
+                // # is 35
+                if desired_column % width == current_column {
+                    trees += 1;
+                }
+                current_column += 1;
+            }
+            46 => {
+                // . is 46
+                current_column += 1;
+            }
+            10 => {
+                // 10 is \n
+                if width == 1 {
+                    width = current_column;
+                }
+                current_column = 0;
+                desired_column += 3;
+            }
+            c => panic!("Unrecognized char: {}", c),
+        };
+    }
+    trees
+}
+
 #[aoc(day3, part2, map)]
 pub fn part2(map: &Map) -> usize {
     fn get_trees_for_slope(map: &Map, right_step: usize, down_step: usize) -> usize {
@@ -124,10 +223,46 @@ mod tests {
     }
 
     #[test]
-    fn verify_part1() {
+    fn verify_part1_perf() {
         let input = include_str!("../input/2020/day3.txt");
 
         let result = part1_perf(input);
+
+        assert_eq!(result, 171)
+    }
+
+    #[test]
+    fn verify_part1_perf_enumerate() {
+        let input = include_str!("../input/2020/day3.txt");
+
+        let result = part1_perf_enumerate(input);
+
+        assert_eq!(result, 171)
+    }
+
+    #[test]
+    fn verify_part1_perf_bytes() {
+        let input = include_bytes!("../input/2020/day3.txt");
+
+        let result = part1_perf_bytes(input);
+
+        assert_eq!(result, 171)
+    }
+
+    #[test]
+    fn verify_part1_perf_bytes_split() {
+        let input = include_bytes!("../input/2020/day3.txt");
+
+        let result = part1_perf_bytes_split(input);
+
+        assert_eq!(result, 171)
+    }
+
+    #[test]
+    fn verify_part1_perf_bytes_modulus() {
+        let input = include_bytes!("../input/2020/day3.txt");
+
+        let result = part1_perf_bytes_modulus(input);
 
         assert_eq!(result, 171)
     }
